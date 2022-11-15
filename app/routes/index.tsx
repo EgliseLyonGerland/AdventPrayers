8b8684d4849638d430bd862f384b5a725c0d0fd8
Nicolas Bazille <oltodo@msn.com>
2022-11-15 23:49:10 +0100

    Create a draw

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,13 +1,15 @@
-import { useLoaderData } from "@remix-run/react";
+import { Form, useLoaderData } from "@remix-run/react";
+import type { ActionFunction } from "@remix-run/server-runtime";
 import { json } from "@remix-run/server-runtime";
 import { prisma } from "~/db.server";
 
-// const year = new Date().getFullYear();
-const year = 2021;
+const year = new Date().getFullYear();
 
-export async function loader() {
-  const draw = await prisma.draw.findUnique({
-    where: { year },
+async function getDraw() {
+  return prisma.draw.findUnique({
+    where: {
+      year,
+    },
     select: {
       year: true,
       players: {
@@ -30,27 +32,53 @@ export async function loader() {
       },
     },
   });
-
-  return json({ draw });
 }
 
+export async function loader() {
+  return json(await getDraw());
+}
+
+export const action: ActionFunction = async ({ request }) => {
+  let formData = await request.formData();
+
+  switch (formData.get("_action")) {
+    case "create":
+      await prisma.draw.create({ data: { year } });
+      break;
+  }
+
+  return json(await getDraw());
+};
+
 export default function Index() {
-  const { draw } = useLoaderData<typeof loader>();
+  const draw = useLoaderData<typeof loader>();
 
   return (
     <main className="container mx-auto py-8">
       <h1 className="text-4xl font-bold">En Avent la prière ! {year}</h1>
 
-      <div className="mt-4 grid">
-        {draw?.players.map(({ person, assigned }) => (
-          <div key={person.id}>
-            <b>
-              {person.firstName} {person.lastName}
-            </b>{" "}
-            {"=>"} {assigned.firstName} {assigned.lastName}
-          </div>
-        ))}
-      </div>
+      {draw ? (
+        <div className="mt-4 grid">
+          {draw?.players.length
+            ? draw.players.map(({ person, assigned }) => (
+                <div key={person.id}>
+                  <b>
+                    {person.firstName} {person.lastName}
+                  </b>{" "}
+                  {"=>"} {assigned.firstName} {assigned.lastName}
+                </div>
+              ))
+            : "Nobody"}
+        </div>
+      ) : (
+        <div className="mt-4">
+          <Form method="post">
+            <button className="btn" type="submit" name="_action" value="create">
+              Create the draw
+            </button>
+          </Form>
+        </div>
+      )}
     </main>
   );
 }
