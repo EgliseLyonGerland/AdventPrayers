01953ae7f49c2687bfb3fe086ce8e1f1edb603a9
Nicolas Bazille <oltodo@msn.com>
2022-11-15 22:40:37 +0100

    Introduce draw page

--- app/root.tsx
+++ app/root.tsx
@@ -35,7 +35,7 @@ export default function App() {
         <Meta />
         <Links />
       </head>
-      <body className="h-full">
+      <body className="h-full bg-blue-900 text-white">
         <Outlet />
         <ScrollRestoration />
         <Scripts />
