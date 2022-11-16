974e99d5fafc6e417ad2c4cb2b2c9140b2de6b4f
Nicolas Bazille <oltodo@msn.com>
2022-11-16 01:57:22 +0100

    Add a person

---
+++ app/components/personSelector.tsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { Combobox } from "@headlessui/react";
+
+type Data = {
+  id: string;
+  firstName: string;
+  lastName: string;
+};
+
+type Props = {
+  persons: Data[];
+  onSelect: (person: Data) => void;
+};
+
+function PersonSelector({ persons, onSelect }: Props) {
+  const [query, setQuery] = useState("");
+
+  const filteredPersons =
+    query === ""
+      ? persons
+      : persons.filter((person) => {
+          const name =
+            `${person.firstName} ${person.lastName}`.toLocaleLowerCase();
+          return name.includes(query.toLowerCase());
+        });
+
+  return (
+    <Combobox nullable onChange={onSelect}>
+      <Combobox.Input
+        className="input-bordered input-primary input w-full max-w-xs"
+        onChange={(event) => setQuery(event.target.value)}
+        placeholder="Ajouter une personne"
+        autoComplete="off"
+      />
+      <Combobox.Options>
+        {filteredPersons.map((person) => (
+          <Combobox.Option key={person.id} value={person}>
+            {person.firstName} {person.lastName}
+          </Combobox.Option>
+        ))}
+      </Combobox.Options>
+    </Combobox>
+  );
+}
+
+export default PersonSelector;
--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,8 +1,9 @@
-import { Form, useLoaderData } from "@remix-run/react";
-import type { ActionFunction } from "@remix-run/server-runtime";
-import { json } from "@remix-run/server-runtime";
-import { redirect } from "@remix-run/node";
+import { Form, useLoaderData, useSubmit } from "@remix-run/react";
+import type { ActionFunction } from "@remix-run/node";
+import { json, redirect } from "@remix-run/node";
 import { prisma } from "~/db.server";
+import PersonSelector from "~/components/personSelector";
+import { useRef } from "react";
 
 const year = new Date().getFullYear();
 
@@ -35,8 +36,21 @@ async function getDraw() {
   });
 }
 
+async function getPersons() {
+  return prisma.person.findMany({
+    select: {
+      id: true,
+      firstName: true,
+      lastName: true,
+    },
+  });
+}
+
 export async function loader() {
-  return json(await getDraw());
+  const draw = await getDraw();
+  const persons = await getPersons();
+
+  return json({ draw, persons });
 }
 
 export const action: ActionFunction = async ({ request }) => {
@@ -49,48 +63,112 @@ export const action: ActionFunction = async ({ request }) => {
 
     case "delete":
       await prisma.draw.delete({ where: { year } });
+      break;
+
+    case "addPerson":
+      const draw = await prisma.draw.findUnique({
+        where: { year },
+        select: { players: { select: { person: { select: { id: true } } } } },
+      });
+
+      if (!draw) {
+        break;
+      }
+
+      const personId = `${formData.get("id")}`;
+
+      if (!personId) {
+        break;
+      }
+
+      await prisma.player.create({
+        data: {
+          drawYear: year,
+          personId,
+        },
+      });
+      break;
   }
 
   return redirect("/");
 };
 
+function Players({
+  players,
+}: {
+  players: NonNullable<Awaited<ReturnType<typeof getDraw>>>["players"];
+}) {
+  if (players.length === 0) {
+    return <span> Nobody for now :(</span>;
+  }
+
+  return (
+    <div>
+      {players.map(({ person, assigned }) => (
+        <div key={person.id}>
+          <b>{`${person.firstName} ${person.lastName}`}</b>
+          {assigned && `=> ${assigned.firstName} ${assigned.lastName}`}
+        </div>
+      ))}
+    </div>
+  );
+}
+
 export default function Index() {
-  const draw = useLoaderData<typeof loader>();
+  const { draw, persons } = useLoaderData<typeof loader>();
+  const submit = useSubmit();
+  const addPersonFormRef = useRef<HTMLFormElement>(null);
 
   return (
     <main className="container mx-auto py-8">
       <h1 className="text-4xl font-bold">En Avent la prière ! {year}</h1>
-      <Form method="post">
-        {draw ? (
-          <div className="mt-4 grid">
-            {draw?.players.length
-              ? draw.players.map(({ person, assigned }) => (
-                  <div key={person.id}>
-                    <b>
-                      {person.firstName} {person.lastName}
-                    </b>
-                    {"=>"} {assigned.firstName} {assigned.lastName}
-                  </div>
-                ))
-              : "Nobody"}
 
-            <button
-              className="btn-error btn"
-              type="submit"
-              name="_action"
-              value="delete"
-            >
-              Delete
-            </button>
-          </div>
-        ) : (
+      {draw ? (
+        <div className="mt-4">
+          <Players players={draw.players} />
+
           <div className="mt-4">
+            <Form method="post" ref={addPersonFormRef}>
+              <input type="hidden" name="_action" value="addPerson" />
+
+              <PersonSelector
+                persons={persons}
+                onSelect={(person) => {
+                  if (!addPersonFormRef.current) {
+                    return;
+                  }
+
+                  const formData = new FormData(addPersonFormRef.current);
+                  formData.set("id", person.id);
+
+                  submit(formData, { method: "post" });
+                }}
+              ></PersonSelector>
+            </Form>
+          </div>
+
+          <div className="mt-16 flex">
+            <Form method="post">
+              <button
+                className="btn-error btn"
+                type="submit"
+                name="_action"
+                value="delete"
+              >
+                Delete the draw
+              </button>
+            </Form>
+          </div>
+        </div>
+      ) : (
+        <div className="mt-4">
+          <Form method="post">
             <button className="btn" type="submit" name="_action" value="create">
               Create the draw
             </button>
-          </div>
-        )}
-      </Form>
+          </Form>
+        </div>
+      )}
     </main>
   );
 }
