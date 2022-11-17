919dd4e56c07bc35ce418d5c5601f5a40d5ade68
Nicolas Bazille <oltodo@msn.com>
2022-11-18 00:20:37 +0100

    Bump deps

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,12 @@ import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
+declare module "vite" {
+  interface UserConfig {
+    test?: any;
+  }
+}
+
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   test: {
