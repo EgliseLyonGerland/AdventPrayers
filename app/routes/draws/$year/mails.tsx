42b81485c8e9f5df659b80fc38473189bce63824
Nicolas Bazille <oltodo@msn.com>
2022-11-25 09:34:40 +0100

    Mails: add placeholder in search input

--- app/routes/draws/$year/mails.tsx
+++ app/routes/draws/$year/mails.tsx
@@ -156,6 +156,7 @@ const Mails = () => {
           onChange={(event) => {
             setSearch(kebabCase(event.target.value));
           }}
+          placeholder="Recherche"
           type="text"
           value={search}
         />
