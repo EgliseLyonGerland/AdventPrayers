07f50fa3a31e8c5d3afe65cdadec9523b38a7ab9
Nicolas Bazille <oltodo@msn.com>
2023-10-23 18:16:05 +0200

    style(auth/login): improve design

--- app/routes/_auth.login.tsx
+++ app/routes/_auth.login.tsx
@@ -4,7 +4,8 @@ import type {
   MetaFunction,
 } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, Link, useActionData, useSearchParams } from "@remix-run/react";
+import { Form, useActionData, useSearchParams } from "@remix-run/react";
+import { Variants, motion } from "framer-motion";
 import { useEffect, useRef } from "react";
 
 import { verifyLogin } from "~/models/user.server";
@@ -64,6 +65,27 @@ export const action = async ({ request }: ActionFunctionArgs) => {
 
 export const meta: MetaFunction = () => [{ title: "Login" }];
 
+const containerVariants: Variants = {
+  hidden: {
+    opacity: 0,
+    y: 1000,
+  },
+  show: {
+    opacity: 1,
+    y: 0,
+
+    transition: {
+      delayChildren: 0.5,
+      staggerChildren: 0.1,
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+};
+
 export default function LoginPage() {
   const [searchParams] = useSearchParams();
   const redirectTo = searchParams.get("redirectTo") || "/notes";
@@ -80,101 +102,86 @@ export default function LoginPage() {
   }, [actionData]);
 
   return (
-    <div className="flex min-h-full flex-col justify-center">
-      <div className="mx-auto w-full max-w-md px-8">
-        <Form className="space-y-6" method="post">
+    <Form
+      className="flex flex-1 items-center justify-center gap-4 text-base-content"
+      method="post"
+    >
+      <motion.div
+        animate="show"
+        className="mx-auto w-full max-w-md space-y-8 rounded-xl border border-base-content/10 bg-base-200 p-8 px-12 shadow-xl"
+        initial="hidden"
+        variants={containerVariants}
+      >
+        <motion.div className="flex flex-col gap-2" variants={itemVariants}>
+          <label htmlFor="email">Email address</label>
           <div>
-            <label
-              className="block text-sm font-medium text-gray-700"
-              htmlFor="email"
-            >
-              Email address
-            </label>
-            <div className="mt-1">
-              <input
-                aria-describedby="email-error"
-                aria-invalid={actionData?.errors?.email ? true : undefined}
-                autoComplete="email"
-                // eslint-disable-next-line jsx-a11y/no-autofocus
-                autoFocus={true}
-                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-                id="email"
-                name="email"
-                ref={emailRef}
-                required
-                type="email"
-              />
-              {actionData?.errors?.email ? (
-                <div className="pt-1 text-red-700" id="email-error">
-                  {actionData.errors.email}
-                </div>
-              ) : null}
-            </div>
+            <input
+              aria-describedby="email-error"
+              aria-invalid={actionData?.errors?.email ? true : undefined}
+              autoComplete="email"
+              // eslint-disable-next-line jsx-a11y/no-autofocus
+              autoFocus={true}
+              className="input w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              id="email"
+              name="email"
+              ref={emailRef}
+              required
+              type="email"
+            />
+            {actionData?.errors?.email ? (
+              <div className="pt-1 text-red-700" id="email-error">
+                {actionData.errors.email}
+              </div>
+            ) : null}
           </div>
+        </motion.div>
 
+        <motion.div className="flex flex-col gap-2" variants={itemVariants}>
+          <label htmlFor="password">Password</label>
           <div>
-            <label
-              className="block text-sm font-medium text-gray-700"
-              htmlFor="password"
-            >
-              Password
-            </label>
-            <div className="mt-1">
-              <input
-                aria-describedby="password-error"
-                aria-invalid={actionData?.errors?.password ? true : undefined}
-                autoComplete="current-password"
-                className="w-full rounded border border-gray-500 px-2 py-1 text-lg"
-                id="password"
-                name="password"
-                ref={passwordRef}
-                type="password"
-              />
-              {actionData?.errors?.password ? (
-                <div className="pt-1 text-red-700" id="password-error">
-                  {actionData.errors.password}
-                </div>
-              ) : null}
-            </div>
+            <input
+              aria-describedby="password-error"
+              aria-invalid={actionData?.errors?.password ? true : undefined}
+              autoComplete="current-password"
+              className="input w-full rounded border border-gray-500 px-2 py-1 text-lg"
+              id="password"
+              name="password"
+              ref={passwordRef}
+              type="password"
+            />
+            {actionData?.errors?.password ? (
+              <div className="pt-1 text-red-700" id="password-error">
+                {actionData.errors.password}
+              </div>
+            ) : null}
           </div>
+        </motion.div>
 
-          <input name="redirectTo" type="hidden" value={redirectTo} />
-          <button
-            className="w-full rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600 focus:bg-blue-400"
-            type="submit"
-          >
-            Log in
-          </button>
-          <div className="flex items-center justify-between">
-            <div className="flex items-center">
-              <input
-                className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
-                id="remember"
-                name="remember"
-                type="checkbox"
-              />
-              <label
-                className="ml-2 block text-sm text-gray-900"
-                htmlFor="remember"
-              >
-                Remember me
-              </label>
-            </div>
-            <div className="text-center text-sm text-gray-500">
-              Don‘t have an account?{" "}
-              <Link
-                className="text-blue-500 underline"
-                to={{
-                  pathname: "/join",
-                  search: searchParams.toString(),
-                }}
-              >
-                Sign up
-              </Link>
-            </div>
+        <input name="redirectTo" type="hidden" value={redirectTo} />
+        <motion.button
+          className="btn btn-secondary btn-outline w-full rounded px-4 py-2"
+          type="submit"
+          variants={itemVariants}
+        >
+          Sign in
+        </motion.button>
+        <motion.div
+          className="flex items-center justify-between"
+          variants={itemVariants}
+        >
+          <div className="flex items-center">
+            <input
+              className="h-4 w-4 rounded border-gray-300 text-blue-600 focus:ring-blue-500"
+              id="remember"
+              name="remember"
+              type="checkbox"
+            />
+            <label className="ml-2 block text-sm" htmlFor="remember">
+              Remember me
+            </label>
           </div>
-        </Form>
-      </div>
-    </div>
+        </motion.div>
+      </motion.div>
+    </Form>
   );
 }
