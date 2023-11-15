3d66c7d505d4692f66d96483348ca33bd8fac198
Nicolas Bazille <oltodo@msn.com>
2023-11-15 01:15:41 +0100

    feat(me): rework page

--- app/routes/me.$id.tsx
+++ app/routes/me.$id.tsx
@@ -6,13 +6,16 @@ import {
   redirect,
 } from "@remix-run/node";
 import { useLoaderData } from "@remix-run/react";
+import clsx from "clsx";
 import { useRef } from "react";
 import invariant from "tiny-invariant";
 
 import AdminRegistationDeleted from "~/components/emails/adminRegistationDeleted";
 import UnregisteredEmail from "~/components/emails/unregistered";
-import SimplePage from "~/components/simplePage";
-import { AppName, AppNameQuoted } from "~/config";
+import FemaleIcon from "~/components/icons/FemaleIcon";
+import MaleIcon from "~/components/icons/MaleIcon";
+import Logo from "~/components/logo";
+import { AppName } from "~/config";
 import { deletePlayer, getCurrentDraw } from "~/models/draw.server";
 import { getPerson } from "~/models/person.server";
 import { genderize, getCurrentYear } from "~/utils";
@@ -91,23 +94,110 @@ export default function Me() {
   const modalRef = useRef<HTMLDialogElement>(null);
 
   return (
-    <SimplePage heading={`👋 Hey ${person.firstName} !`}>
-      <div>
-        Tu es {genderize("inscrit", person.gender)} pour l’édition {draw.year}{" "}
-        de {AppNameQuoted}.
+    <>
+      <div className="mx-auto flex h-screen max-w-[1200px] flex-col items-center gap-8 overflow-y-hidden px-4 py-8 md:gap-12 md:p-12">
+        <Logo className="h-[15vw] md:h-28" />
+        <div
+          className={clsx(
+            "no-scrollbar flex max-w-full flex-1 snap-mandatory overflow-y-hidden",
+            "max-[900px]:snap-x max-[900px]:gap-8 max-[900px]:overflow-x-scroll",
+            "divide-neutral min-[900px]:divide-x",
+          )}
+        >
+          <div
+            className={clsx(
+              "rounded-box flex flex-1 snap-center flex-col gap-12 overflow-y-auto bg-base-200 p-8 text-xl",
+              "max-[900px]:min-w-full",
+              "min-[900px]:rounded-e-none",
+            )}
+          >
+            <h2 className="text-xl font-bold uppercase">Tes informations</h2>
+            <div className="flex flex-col">
+              <div>
+                {person.firstName} {person.lastName}{" "}
+                <span className="ml-2">
+                  {person.gender === "male" ? (
+                    <MaleIcon className="inline-block h-4 opacity-60" />
+                  ) : (
+                    <FemaleIcon className="inline-block h-4 opacity-60" />
+                  )}
+                </span>
+              </div>
+              <div className="opacity-60">
+                {person.email}{" "}
+                <span className="badge badge-neutral ml-2">{person.age}</span>
+              </div>
+              <div className="mt-8">
+                <div className="mb-2 text-lg font-bold">À ton sujet</div>
+                <div className="opacity-60">
+                  {person.bio ? person.bio : <i>Tu n’as rien écrit sur toi</i>}
+                </div>
+              </div>
+              <div className="mt-8">
+                <div className="mb-2 text-lg font-bold">Ta photo</div>
+                <div>
+                  {person.picture ? (
+                    <img
+                      alt={`${person.firstName} ${person.lastName}`}
+                      className="aspect-square h-28"
+                      src={`/uploads/${person.picture}`}
+                    />
+                  ) : (
+                    <i>Tu n’as pas fournit de photo</i>
+                  )}
+                </div>
+              </div>
+            </div>
+            <div className="tooltip mt-auto" data-tip="Bientôt, promis !">
+              <button
+                className="btn btn-secondary btn-outline w-full"
+                disabled={true}
+              >
+                Modifier
+              </button>
+            </div>
+          </div>
+          <div
+            className={clsx(
+              "rounded-box flex flex-1 snap-center flex-col gap-12 overflow-y-auto bg-base-200 p-8 text-xl",
+              "max-[900px]:min-w-full",
+              "min-[900px]:rounded-l-none",
+            )}
+          >
+            <h2 className="text-xl font-bold uppercase">Pour qui pries-tu ?</h2>
+
+            {draw.drawn ? (
+              "todo"
+            ) : (
+              <>
+                <div className="mb-8">
+                  <p className="mb-6 italic">
+                    L’opération n’a pas encore démarré. Je ne peux pas encore te
+                    dire à qui devront s’adresser tes prières. Tu connaitras le
+                    nom de la personne le premier jour de l’Avent, c’est à dire
+                    le 3 décembre prochain. Tu pourras le voir ici-même et je te
+                    l’enverrai aussi par email, alors surveille bien ta boîte de
+                    réception (et de spams aussi) ce jour là.
+                  </p>
+                  <p className="italic">
+                    Si tu le souhaites, tu peux toujours te désincrire en
+                    cliquant sur le bouton ci-dessous. Sache que ça ne sera plus
+                    possible une fois le tirage lancé.
+                  </p>
+                </div>
+                <button
+                  className="btn btn-neutral mt-auto w-full"
+                  onClick={() => {
+                    modalRef.current?.showModal();
+                  }}
+                >
+                  Me désinscrire
+                </button>
+              </>
+            )}
+          </div>
+        </div>
       </div>
-      <div>
-        L’oépration n’a pas encore démarré, tu peux encore te désinscrire si tu
-        le souhaites.
-      </div>
-      <button
-        className="btn btn-secondary btn-outline md:btn-lg"
-        onClick={() => {
-          modalRef.current?.showModal();
-        }}
-      >
-        Me désinscrire 🥲
-      </button>
 
       <dialog className="modal" ref={modalRef}>
         <div className="modal-box">
@@ -116,8 +206,11 @@ export default function Me() {
           </h3>
           <div>Es-tu sûr de vouloir te désinscrire{" "}?</div>
           <div className="modal-action">
-            <form method="post">
-              <button className="btn">Oui oui, je suis sûr !</button>
+            <form className="flex gap-2" method="post">
+              <button className="btn btn-ghost" formMethod="dialog">
+                Attends je réfléchis...
+              </button>
+              <button className="btn">Je suis sûr !</button>
             </form>
           </div>
         </div>
@@ -126,6 +219,6 @@ export default function Me() {
           <button>close</button>
         </form>
       </dialog>
-    </SimplePage>
+    </>
   );
 }
